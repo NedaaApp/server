3f31621332c00e19db267c85a437a91751f8de83
Fahad <42780409+F2had@users.noreply.github.com>
2022-06-23 15:24:22 +0300

    Add rate limit middlewear

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,20 @@
+import { APP_INTERCEPTOR } from '@nestjs/core';
 import { Module } from '@nestjs/common';
+import { RateLimiterModule, RateLimiterInterceptor } from 'nestjs-fastify-rate-limiter';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
 @Module({
-  imports: [],
+  imports: [RateLimiterModule.register({
+    points: 100,
+    duration: 60,
+    errorMessage: 'Too many requests',
+    keyPrefix: 'rate-limiter',
+})],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [AppService, {
+    provide: APP_INTERCEPTOR,
+    useClass: RateLimiterInterceptor,
+  }],
 })
 export class AppModule {}
