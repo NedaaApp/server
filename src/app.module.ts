7a394df296e7a8ee502361bca77d6cf0240a8965
Fahad <42780409+F2had@users.noreply.github.com>
2022-06-25 17:25:33 +0300

    Add configure module to use env variables

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,6 +1,7 @@
 import { Module } from '@nestjs/common';
 import { APP_GUARD } from '@nestjs/core';
 import { ThrottlerGuard, ThrottlerModule } from '@nestjs/throttler';
+import { ConfigModule } from '@nestjs/config';
 import { CalendarService } from './calendar/calendar.service';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
@@ -8,6 +9,9 @@ import { CalendarController } from './calendar/calendar.controller';
 
 @Module({
   imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
     ThrottlerModule.forRoot({
       ttl: 60,
       limit: 25,
