e7111daea157ac8d96282162094219170c48190a
Fahad <42780409+F2had@users.noreply.github.com>
2025-01-16 03:52:38 +0300

    refactor: Emit proxy form  prayer service name

--- src/prayer-times/prayer-times.module.ts
+++ src/prayer-times/prayer-times.module.ts
@@ -1,11 +1,11 @@
 import { Module } from '@nestjs/common';
-import { PrayerTimesController } from './controllers/prayer-times.controller';
-import { AladhanService } from './services/aladhan/aladhan.service';
-import { ProviderService } from './services/provider/provider.service';
-import { PrayerTimesProxyService } from './services/prayer-times-proxy/prayer-times-proxy.service';
+import { PrayerTimesController } from '@/prayer-times/controllers/prayer-times.controller';
+import { AladhanService } from '@/prayer-times/services/aladhan/aladhan.service';
+import { ProviderService } from '@/prayer-times/services/provider/provider.service';
+import { PrayerTimesService } from '@/prayer-times/services/prayer-times.service';
 
 @Module({
   controllers: [PrayerTimesController],
-  providers: [ProviderService, PrayerTimesProxyService, AladhanService],
+  providers: [ProviderService, PrayerTimesService, AladhanService],
 })
 export class PrayerTimesModule {}
