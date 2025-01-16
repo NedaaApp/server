7acfc4d94a3ad232c74d0bfd3dcbdbaab1899114
Fahad <42780409+F2had@users.noreply.github.com>
2025-01-16 08:33:07 +0300

    feat: Fetch and process the timing from aladhan provider

--- src/prayer-times/services/aladhan/aladhan.service.spec.ts
+++ src/prayer-times/services/aladhan/aladhan.service.spec.ts
@@ -1,11 +1,13 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { AladhanService } from './aladhan.service';
+import { HttpModule } from '@nestjs/axios';
 
 describe('AladhanService', () => {
   let service: AladhanService;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
+      imports: [HttpModule],
       providers: [AladhanService],
     }).compile();
 
